5ff0b0c9b6606187a4b687d2c8d503790876cb2a
ArdaUstaoglu <113902268+ArdaUstaoglu@users.noreply.github.com>
2022-10-05 15:49:04 +0300

    Initial commit

---
+++ mantarKontrolSistemi.ino
@@ -0,0 +1,11 @@
+#include "mantarSensor.h"
+MantarSensor mantarSensor(A1, 11, 8, 6);
+void setup() {
+  Serial.begin(9600);
+  mantarSensor.setup();
+}
+
+void loop() {
+Serial.println(mantarSensor.temp());
+mantarSensor.attach();
+}
---
+++ mantarSensor.h
@@ -0,0 +1,41 @@
+class MantarSensor {
+
+
+  public:
+    int tempDownPin = 0;
+    int tempNormalPin = 0;
+    int tempUpPin = 0;
+    int tempSensorPin = 0;
+    double tempValue = 0;
+    double minTemp=22.2;
+    double maxTemp=26.8;
+    MantarSensor(int tsp, int tdp, int tnp, int tup) {
+      tempSensorPin = tsp;
+      tempDownPin = tdp;
+      tempNormalPin = tnp;
+      tempUpPin = tup;
+
+    }
+    void setup() {
+      pinMode(tempDownPin, OUTPUT);
+      pinMode(tempNormalPin, OUTPUT);
+      pinMode(tempDownPin, OUTPUT);
+
+
+    }
+    double temp() {
+      tempValue = analogRead(tempSensorPin);
+      tempValue = tempValue / 6.82;
+      tempValue = tempValue - 50;
+      return tempValue;
+    }
+    void attach() {
+      temp();
+      if(tempValue<minTemp) digitalWrite(tempDownPin,HIGH);else digitalWrite(tempDownPin,LOW);
+      if(tempValue>maxTemp) digitalWrite(tempUpPin,HIGH);else digitalWrite(tempUpPin,LOW);
+      if(tempValue>=minTemp&&tempValue<=maxTemp) digitalWrite(tempNormalPin,HIGH);else digitalWrite(tempNormalPin,LOW);
+    }
+  private:
+
+
+};
